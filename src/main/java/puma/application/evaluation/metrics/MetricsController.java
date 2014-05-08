0cd878c9e0d15af4ade5baa11b5fd1aa62a9efcc
Maarten Decat <maarten.decat@gmail.com>
2014-05-08 14:59:15 +0200

    added graphite reporting

--- src/main/java/puma/application/evaluation/metrics/MetricsController.java
+++ src/main/java/puma/application/evaluation/metrics/MetricsController.java
@@ -1,5 +1,8 @@
 package puma.application.evaluation.metrics;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import puma.util.timing.TimerFactory;
 
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -28,5 +34,15 @@ public class MetricsController {
 	@RequestMapping(value = "/metrics/clear", method = RequestMethod.GET)
 	public @ResponseBody void clear() {
 		TimerFactory.getInstance().resetAllTimers();
+		
+		// connect metrics to the Graphite server
+		final Graphite graphite = new Graphite(new InetSocketAddress("172.16.4.2", 2003));
+		final GraphiteReporter reporter = GraphiteReporter.forRegistry(TimerFactory.getInstance().getMetricRegistry())
+		                                                  .prefixedWith("puma-application")
+		                                                  .convertRatesTo(TimeUnit.SECONDS)
+		                                                  .convertDurationsTo(TimeUnit.MILLISECONDS)
+		                                                  .filter(MetricFilter.ALL)
+		                                                  .build(graphite);
+		reporter.start(10, TimeUnit.SECONDS);
 	}
 }
